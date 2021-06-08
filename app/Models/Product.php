9e7275b55fe12dce55e3efe37f271770046b2e87
Michelle Sanchez Miranda <michelmiranda3786@gmail.com>
2021-06-08 12:27:28 -0500

    Creacion Modelos

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -19,67 +19,6 @@ class Product extends Model
         return $this->belongsTo(Trademark::class);
     }
 
-    //alcanze con el modelo Coupons_product
-    public function coupon_product()
-    {
-        return $this->belongsTo(Coupon_product::class);
-    }
-
-    //alcanze con el modelo Coupons
-    public function coupon()
-    {
-        return $this->belongsTo(Coupon::class);
-    }
-
-
-    //alcanze con el modelo Photos
-    public function photo()
-    {
-        return $this->belongsToMany(Photo::class,'photo_product','photo_id', 'product_id');
-    }
-
-    //alcanze con el modelo Photo_product
-    public function photo_product()
-    {
-        return $this->belongsTo(Photo_product::class);
-    }
-
-    //alcanze con el modelo Category_product
-    public function category_product()
-    {
-        return $this->belongsTo(Category_product::class);
-    }
-
-    //alcanze con el modelo Category
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
-
-    //alcanze con el modelo Product_user
-    public function product_user()
-    {
-        return $this->belongsTo(Product_user::class);
-    }
-
-    //alcanze con el modelo User
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    //alcanze con el modelo Order_detail
-    public function order_det()
-    {
-        return $this->belongsTo(Order_detail::class);
-    }
-
-    //alcanze con el modelo Order
-    public function order()
-    {
-        return $this->belongsTo(Order::class);
-    }
-
     //alcanze con el modelo Product_variation
     public function prod_vari()
     {
