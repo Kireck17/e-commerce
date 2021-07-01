befb1f5a0f606a9e0026cb9622a9586a7d3259b5
Luis Angel <anggz1236@gmail.com>
2021-07-01 16:04:03 -0500

    Navigation menu version 2

--- resources/views/livewire/nav/navigation.blade.php
+++ resources/views/livewire/nav/navigation.blade.php
@@ -1,18 +1,25 @@
 <div>
     <div class="hidden sm:flex flex flex-wrap bg-gray-100 w-full h-screen">
         <div class="w-full bg-white rounded p-3 shadow-lg">
+            {{--
             <div class="flex items-center space-x-4 p-2 mb-5">
                 <img class="h-12 rounded-full" src="http://www.gravatar.com/avatar/2acfb745ecf9d4dccb3364752d17f65f?s=260&d=mp" alt="James Bhatta">
                 <div>
-                    <h4 class="font-semibold text-lg text-gray-700 capitalize font-poppins tracking-wide">James Bhatta</h4>
+                    <h4 class="font-semibold text-lg text-gray-700 capitalize font-poppins tracking-wide">Usuario</h4>
                     <span class="text-sm tracking-wide flex items-center space-x-1">
                         <svg class="h-4 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-                        </svg><span class="text-gray-600">Verified</span>
+                        </svg><span class="text-gray-600">Verificado</span>
                     </span>
                 </div>
             </div>
-            <ul class="space-y-2 text-sm">
+            --}}
+            <div class="px-3 flex justify-center items-center ">
+                <a href="">
+                    <img class="h-16 w-auto" src="{{asset('storage/logos/originalsaxcar.png')}}" alt="">
+                </a>
+            </div>
+            <ul class="space-y-2 text-sm mt-5">
                 <li>
                     <a href="#" class="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-gray-200 bg-gray-200 focus:shadow-outline">
                         <span class="text-gray-600">
@@ -20,7 +27,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                             </svg>
                         </span>
-                        <span>Dashboard</span>
+                        <span>Inicio</span>
                     </a>
                 </li>
                 <li>
@@ -30,7 +37,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
                             </svg>
                         </span>
-                        <span>Notifications</span>
+                        <span>Notificaciones</span>
                     </a>
                 </li>
                 <li>
@@ -40,7 +47,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path>
                             </svg>
                         </span>
-                        <span>Personal messages</span>
+                        <span>Personal </span>
                     </a>
                 </li>
                 <li>
@@ -50,7 +57,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                             </svg>
                         </span>
-                        <span>My profile</span>
+                        <span>Mi cuenta</span>
                     </a>
                 </li>
                 <li>
