d16678dddc5642f10652ee63d708f4c3af4fca45
OrlandoWT15 <gouiterra@gmail.com>
2021-07-28 11:18:23 -0500

    Agregando variacion una modal por aparte

---
+++ app/Http/Livewire/Admin/Show/Variation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Show;
+
+use Livewire\Component;
+
+class Variation extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.show.variation');
+    }
+}
